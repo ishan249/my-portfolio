d2b8d47e603fae51379f7e45fbf7d3a50b68e60d
ishan249 <249patelishan@gmail.com>
2023-01-25 00:18:24 +0530

    changed basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
 
-      <BrowserRouter>
+      <BrowserRouter basename="/index.html">
       <Routes>
         <Route path = "/" element={<Homepage/>}/>
         <Route path = "/About" element={<About/>}/>
