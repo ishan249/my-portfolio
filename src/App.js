0fed406facd6c367b1acce8f3532a7c150f6361f
ishan249 <249patelishan@gmail.com>
2023-01-25 00:43:44 +0530

    changed the homepage location

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
 
-      <BrowserRouter basename="/index.html">
+      <BrowserRouter>
       <Routes>
         <Route path = "/" element={<Homepage/>}/>
         <Route path = "/About" element={<About/>}/>
